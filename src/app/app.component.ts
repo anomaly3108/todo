7d1f9d096254f206dee1e68d7d60ba27414f9f8d
Aviral <aviralsharma205@gmail.com>
2020-07-05 17:31:35 +0530

    changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit{
+  todoArray: string[] = [];
+
+  public form: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.constructForm();
+  }
+
+  constructForm() {
+    this.form = this.fb.group({
+      todo: this.fb.control(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) { return; }
+    this.todoArray.push(this.form.get('todo').value);
+    this.form.reset();
+    console.log(this.todoArray)
+  }
+
+  onDeleteItem(index) {
+    this.todoArray.splice(index, 1);
+  }
+
+
+}
